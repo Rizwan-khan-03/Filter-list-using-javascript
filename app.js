80704336b03848d68b1f7a7c3f9f3aee74c5cf30
Rizwan khan <69905693+Rizwan-khan-03@users.noreply.github.com>
2022-02-16 16:27:00 +0530

    update

--- app.js
+++ app.js
@@ -1,158 +1,241 @@
-// get only unique categories - HARDEST ONE
-// iterate over categories return buttons
-// make sure to select buttons when they are available
+const fruits= [
+  "apple",
+  "apricot",
+  "avocado",
+   "banana",
+   "bell pepper",
+   "bilberry",
+   "blackberry",
+   "blackcurrant",
+   "blood orange",
+  // "blueberry",
+  // "boysenberry",
+  // "breadfruit",
+  // "canary melon",
+  // "cantaloupe",
+  // "cherimoya",
+  // "cherry",
+  // "chili pepper",
+  // "clementine",
+  // "cloudberry",
+  // "coconut",
+  // "cranberry",
+  // "cucumber",
+  // "currant",
+  // "damson",
+  // "date",
+  // "dragonfruit",
+  // "durian",
+  // "eggplant",
+  // "elderberry",
+  // "feijoa",
+  // "fig",
+  // "goji berry",
+  // "gooseberry",
+  // "grape",
+  // "grapefruit",
+  // "guava",
+  // "honeydew",
+  // "huckleberry",
+  // "jackfruit",
+  // "jambul",
+  // "jujube",
+  // "kiwi fruit",
+  // "kumquat",
+  // "lemon",
+  // "lime",
+  // "loquat",
+  // "lychee",
+  // "mandarine",
+  // "mango",
+  // "mulberry",
+  // "nectarine",
+  // "nut",
+  // "olive",
+  // "orange",
+  // "papaya",
+  // "passionfruit",
+  // "peach",
+  // "pear",
+  // "persimmon",
+  // "physalis",
+  // "pineapple",
+  // "plum",
+  // "pomegranate",
+  // "pomelo",
+  // "purple mangosteen",
+  // "quince",
+  // "raisin",
+  // "rambutan",
+  // "raspberry",
+  // "redcurrant",
+  // "rock melon",
+  // "salal berry",
+  // "satsuma",
+  // "star fruit",
+  // "strawberry",
+  // "tamarillo",
+  // "tangerine",
+  // "tomato",
+  // "ugli fruit",
+  // "watermelon"
+]
+var fruitsobject = [
+  {
+   name: 'apple',
+   number:777777 
+ },
+  { 
+   name: 'mango',
+   number:99999
+  },
+  {
+    name: 'graps',
+    number:1111
+ },
+  {
+    name: 'watermelon',
+    number:2222
+ },
+  {
+    name: 'banan',
+    number:333
+  },
+  {
+    name: 'lichi',
+    number:333
+  },
+  {
+    name: 'papaya',
+    number:333
+  },
+  {
+    name: 'kivi',
+    number:333
+  },
+  {
+    name: 'kivi',
+    number:333
+  }
+  ,
+  {
+    name: 'kivi',
+    number:333
+  },
+  {
+    name: 'kivi',
+    number:333
+  }
 
-// items
-const menu = [
-    {
-      id: 1,
-      title: "buttermilk pancakes",
-      category: "breakfast",
-      price: 15.99,
-      img: "./images/item-1.jpeg",
-      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
-    },
-    {
-      id: 2,
-      title: "diner double",
-      category: "lunch",
-      price: 13.99,
-      img: "./images/item-2.jpeg",
-      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
-    },
-    {
-      id: 3,
-      title: "godzilla milkshake",
-      category: "shakes",
-      price: 6.99,
-      img: "./images/item-3.jpeg",
-      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
-    },
-    {
-      id: 4,
-      title: "country delight",
-      category: "breakfast",
-      price: 20.99,
-      img: "./images/item-4.jpeg",
-      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
-    },
-    {
-      id: 5,
-      title: "egg attack",
-      category: "lunch",
-      price: 22.99,
-      img: "./images/item-5.jpeg",
-      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
-    },
-    {
-      id: 6,
-      title: "oreo dream",
-      category: "shakes",
-      price: 18.99,
-      img: "./images/item-6.jpeg",
-      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
-    },
-    {
-      id: 7,
-      title: "bacon overflow",
-      category: "breakfast",
-      price: 8.99,
-      img: "./images/item-7.jpeg",
-      desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
-    },
-    {
-      id: 8,
-      title: "american classic",
-      category: "lunch",
-      price: 12.99,
-      img: "./images/item-8.jpeg",
-      desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
-    },
-    {
-      id: 9,
-      title: "quarantine buddy",
-      category: "shakes",
-      price: 16.99,
-      img: "./images/item-9.jpeg",
-      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
-    },
-    {
-      id: 10,
-      title: "steak dinner",
-      category: "dinner",
-      price: 39.99,
-      img: "./images/item-10.jpeg",
-      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
-    },
-  ];
-  
-  const sectionCenter = document.querySelector(".section-center");
-  const container = document.querySelector(".btn-container");
-  
-  // load items
-  window.addEventListener("DOMContentLoaded", function () {
-    displayMenuItems(menu);
-    displayMenuButtons();
-  });
-  
-  function displayMenuItems(menuItems) {
-    let displayMenu = menuItems.map(function (item) {
-      // console.log(item);
-  
-      return `<article class="menu-item">
-            <img src=${item.img} class="photo" alt=${item.title} />
-            <div class="item-info">
-              <header>
-                <h4>${item.title}</h4>
-                <h4 class="price">$${item.price}</h4>
-              </header>
-              <p class="item-text">
-                ${item.desc}
-              </p>
-            </div>
-          </article>`;
-    });
-    displayMenu = displayMenu.join("");
-  
-    sectionCenter.innerHTML = displayMenu;
-  }
-  
-  function displayMenuButtons() {
-    const categories = menu.reduce(
-      function (values, item) {
-        if (!values.includes(item.category)) {
-          values.push(item.category);
-        }
-        return values;
-      },
-      ["all"]
-    );
-    const categoryBtns = categories
-      .map(function (category) {
-        return `<button class="filter-btn" type="button" data-id=${category}>
-        ${category}
-        </button>`;
-      })
-      .join("");
-    container.innerHTML = categoryBtns;
-    const filterBtns = container.querySelectorAll(".filter-btn");
-    // filter items
-    filterBtns.forEach(function (btn) {
-      btn.addEventListener("click", function (e) {
-        const category = e.currentTarget.dataset.id;
-        const menuCategory = menu.filter(function (menuItem) {
-          // console.log(menuItem.category);
-          if (menuItem.category === category) {
-            return menuItem;
-          }
-        });
-        // console.log(menuCategory);
-        if (category === "all") {
-          displayMenuItems(menu);
+    ];
+    //step-1-- get input value
+    // step-2--submit input value in array
+     let arr=["apple",
+     "apricot",
+     "avocado",];
+
+    var app = document.querySelector('#app');
+    const ul = document.createElement("ul");
+    ul.setAttribute("id", "myUL")
+    const li=document.createElement("li");
+    li.setAttribute("id", "li");
+    
+      //make list with map
+    ul.innerHTML=(fruits.map(function (item) {
+      return '<li>' +item + '</li>';
+    })).join('');
+    app.appendChild(ul);
+
+    var myForm=document.getElementById('myForm');
+      myForm.addEventListener('submit',function(event){
+      event.preventDefault();
+      var formInput=document.getElementById('formInput').value;
+      fruits.push(formInput)
+      //make list with map
+      ul.innerHTML=(fruits.map(function (item) {
+      return '<li>' +item + '</li>';
+    })).join('');
+
+    })
+    // console.log(arr)
+
+
+ 
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+
+    console.log("ul"+ul.innerText)
+    console.log(input)
+    console.log("filter"+filter)
+
+    for (i = 0; i < li.length; i++) {
+        // a = li[i].getElementsByTagName("a")[0]; use while li contain anchor tag as below
+       // txtValue = a.textContent || a.innerText;
+        txtValue = li[i].textContent || li[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter)  > -1) {
+            li[i].style.display ="";
         } else {
-          displayMenuItems(menuCategory);
+            li[i].style.display = "none";
         }
-      });
-    });
-  }
-  
+   
+        
+    }
+}
+ //  function onSubmit(){
+  //  formInput=document.getElementById('formInput');
+  //  inputValue=formInput.value;
+  //  fruits.push(inputValue);
+
+  //  formdata.innerHTML=(fruits.map(function (item) {
+  //   return '<li>' +item + '</li>';
+  // })).join('');
+
+  //  console.log(arr)
+  //  }
+  //  formdata.innerHTML=(arr.map(function (item) {
+  //    return '<li>' +item + '</li>';
+  //  })).join('');
+   
+
+
+    //////form submit for add fruits
+
+// Get the app element
+// var app = document.querySelector('#app');
+// const ul = document.createElement("ul");
+// ul.setAttribute("id", "myUL")
+// const li=document.createElement("li");
+// li.setAttribute("id", "li")
+
+// ul.innerHTML=(arr.map(function (item) {
+// 	return '<li>' +item + '</li>';
+// })).join('');
+
+//////////////////////for next project compete form////////////
+
+// var app = document.querySelector('#app');
+// const ul = document.createElement("ul");
+// ul.setAttribute("id", "myUL")
+// const li=document.createElement("li");
+// li.setAttribute("id", "li")
+// ul.innerHTML=(fruitsobject.map(function (item) {
+//   return '<li>' +item.name + '</li>';
+// })).join('');
+// app.appendChild(ul);
+
+// var myForm=document.getElementById('myForm');
+//   myForm.addEventListener('submit',function(event){
+//   event.preventDefault();
+//   var formInput=document.getElementById('formInput').value;
+//   fruitsobject.push({name:formInput})
+// console.log(arr)
+// ul.innerHTML=(fruitsobject.map(function (item) {
+//   return '<li>' +item.name + '</li>';
+// })).join('');
+
+// })
+//////////////////////for next project compete form////////////
